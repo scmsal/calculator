73554c8447b212d1aed896ad8e2fecb00708c4ed
scmsal <31stephcm@gmail.com>
2023-10-18 10:26:36 -0400

    added conversion calculator

--- script.js
+++ script.js
@@ -18,9 +18,34 @@ function compute() {
     }
 
     function updateRate()
-{
+    {
         var rateval = document.getElementById("rate").value;
         document.getElementById("rate_val").innerText = rateval; 
      }
 
-
+function temperature () {
+        //To convert Celsius to Farenheit
+        //(CEL * 9/5) + 32
+        var c = document.getElementById("Celsius").value;
+        var f = (c * 9/5) + 32
+        document.getElementById("Fahrenheit").value = f 
+        }
+        
+function weight () {
+            //To convert Kilograms to Pounds
+            // kg * 2.2
+            var kg = document.getElementById("kg").value;
+            var lbs = kg * 2.2
+            document.getElementById("lbs").value = lbs 
+            }
+        
+function distance () {
+            //To convert Kilometers to Miles
+            //Distance(Miles) = Distance(Kms) * 0.62137
+            
+            var km = document.getElementById("km").value;
+            var m = km * 0.62137
+            document.getElementById("miles").value = m
+            }
+    
+    
